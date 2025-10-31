9d989d0bc1d8ba7073804e6b169b1e257fa99e15
Kiran Chenna <kiran.chenna@netcracker.com>
2025-10-31 10:56:03 +0530

    adding two new files recursive binary search and executor thread poool

--- src/main/java/org/example/DSA/RecursiveBinarySearch.java
+++ src/main/java/org/example/DSA/RecursiveBinarySearch.java
@@ -1,2 +1,60 @@
-package org.example.DSA;public class RecursiveBinarySearch {
+package org.example.DSA;
+
+import java.util.Arrays;
+
+public class RecursiveBinarySearch {
+
+    public static int binarySearch(int[] nums, int target) {
+        Arrays.sort(nums);
+        return binarySearchRecursion(nums, target, 0, nums.length - 1);
+    }
+
+    public static int linearSearch(int[] nums, int target) {
+        Arrays.sort(nums);
+        return linearSearchRecursion(nums, target, 0);
+    }
+
+
+    public static int linearSearchRecursion(int[] nums, int target, int index) {
+        if(index >= nums.length){
+            return -1;
+        }
+
+        if(nums[index] == target){
+            return index;
+        }
+        return linearSearchRecursion(nums, target, index+1);
+    }
+
+    public static int binarySearchRecursion(int[] nums, int target, int left, int right) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearchRecursion(nums, target, left, mid - 1);
+        } else {
+            return binarySearchRecursion(nums, target, mid + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {89, 12, 44, 55, 1}; //1, 12, 44, 55, 89
+        int target = 12;
+
+        if(binarySearch(nums, target) != -1 )
+            System.out.println("Index values of binary search is: " + binarySearch(nums, target));
+        else
+            System.out.println("No index found in binary..");
+
+        if(linearSearch(nums, target) != -1)
+            System.out.println("Index values of linear search is: "+ linearSearch(nums, target));
+        else
+            System.out.println("No index found in linear..");
+    }
 }
--- src/main/java/org/example/threads/threadPool/ExecutorTreadPoolPractice.java
+++ src/main/java/org/example/threads/threadPool/ExecutorTreadPoolPractice.java
@@ -1,2 +1,31 @@
-package org.example.threads.threadPool;public class ExecutorTreadPoolPractice {
+package org.example.threads.threadPool;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorTreadPoolPractice {
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 1; i <= 8; i++) {
+            executor.submit(() -> {
+                System.out.println("Thread name: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(20000);
+                } catch (Exception e) {
+                    System.out.println("Threads Terminated: " + Thread.currentThread().getName());
+                }
+            });
+        }
+        executor.shutdown();
+        if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+            List<Runnable> pendingTasks = executor.shutdownNow();
+            System.out.println("Number of pending tasks left: " + pendingTasks.size());
+            System.out.println("Pending task at first index: " + pendingTasks.get(0));
+        }
+    }
+
 }
