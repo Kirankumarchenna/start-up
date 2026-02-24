f5efc911c4d6c83215ae4ee914efe5af831d2f31
kirankumarchenna <kiransamuel7287@gmail.com>
2026-02-24 07:22:21 +0530

    orderAgnosticBinary

--- src/main/java/org/practice/DSA/searching/binarySearch/BinarySearch.java
+++ src/main/java/org/practice/DSA/searching/binarySearch/BinarySearch.java
@@ -4,25 +4,37 @@ import java.util.Arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {2, 43, 5, 78, 98};
-        int target = 78;
-        Arrays.sort(arr);
-        int r = binarySearch(arr, target);
-        System.out.println(r);
+        int[] arr = {9, 8, 7, 6, 4, 2, 1, 0};
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int target = 2;
+        int ans = orderAgnosticBinary(arr1, target);
+        System.out.println(ans);
     }
 
-    static int binarySearch(int[] arr, int target){
+    static int orderAgnosticBinary(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
 
         while(start <= end) {
             int mid = start + (end - start) / 2; //due to integer length complexity
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
+            boolean isAsc = arr[start] < arr[end];
+
+            if(target == arr[mid]){
                 return mid;
+            }
+
+            if(isAsc){
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
